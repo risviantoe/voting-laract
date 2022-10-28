bf417f83d7b6564d25f7b0789d465ab5026a6fa1
Edwin Risvianto <erisvianto@gmail.com>
2022-10-28 11:21:45 +0700

    add id in migration table

--- database/migrations/2022_10_26_151615_create_tokens_table.php
+++ database/migrations/2022_10_26_151615_create_tokens_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
             $table->string('token');
             $table->timestamps();
         });
